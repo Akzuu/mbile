99f1b677784772f2b54e8a17933bed356ca68607
unknown <akzuu@iname.com>
2019-05-11 18:47:25 +0300

    Init repo

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+app.get('', (req, res) => {
+	res.render('index',  {
+		title: 'mbile.fi',
+		name: 'Team Tumppi vs *'
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render('pageNotFound', {
+		title: '404',
+		text: 'Page not found!',
+		name: 'Team Tumppi vs *'
+	})
+})
+
+app.listen(port, () => {
+	console.log(`App running on port ${port}.`)
+})
